dc78892b42548899650d4fbc8aa5d8f3dcf542c3
Adam McDaniel <adam.mcdaniel17@gmail.com>
2026-01-29 14:41:36 -0500

    Updated run to dump to stdout not file

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ fi
 
 cd $LSMS_DIR/FePt/
 
-srun -t10 -A csc688 -N1 $LSMS_DIR/build/bin/lsms i_lsms_express_gpu > "$LSMS_DIR/FePt/lsms_output.out" 2> "$LSMS_DIR/FePt/lsms_output.err"
+srun -t10 -A csc688 -N1 $LSMS_DIR/build/bin/lsms i_lsms_express_gpu
